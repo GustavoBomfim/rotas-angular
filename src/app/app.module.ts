6df76f7bce09aac77c811d08b46fd7a2bb266732
GustavoBomfim <gustavosoaresbomfim@hotmail.com>
2022-02-14 10:12:23 -0300

    Definindo rota padrao e wildcard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { FormsModule } from '@angular/forms';
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 //import { CursosModule } from './cursos/cursos.module';
 //import { AlunosModule } from './alunos/alunos.module';
 //import { AlunosComponent } from './alunos/alunos.component';
@@ -26,6 +27,7 @@ import { AlunosGuard } from './guards/alunos.guard';
     //CursosComponent,
     HomeComponent,
     LoginComponent,
+    PaginaNaoEncontradaComponent,
     //AlunosComponent,
     //CursoDetalheComponent,
     //CursoNaoEncontradoComponent
--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -12,6 +12,7 @@ import { Routes, RouterModule } from '@angular/router'
 import { AuthGuard } from './guards/auth.guard';
 import { CursosGuard } from './guards/cursos.guard';
 import { AlunosGuard } from './guards/alunos.guard';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 
 
@@ -28,7 +29,9 @@ const APP_ROUTES: Routes = [
     //{ path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent},
     //{ path: 'naoEncontrado', component: CursoNaoEncontradoComponent },
-    { path: '', component: HomeComponent, canActivate: [AuthGuard]}
+    { path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+    { path: '', redirectTo: '/home', pathMatch: 'full'},
+    { path: '**', component: PaginaNaoEncontradaComponent},
 ];
 
 @NgModule({
